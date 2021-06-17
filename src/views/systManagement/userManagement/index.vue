9be6e0be9580d28f76a1eb11a0bfe17d5485fd60
mwk <2515011987@qq.com>
2021-06-17 17:44:50 +0800

    用户管理界面--->分配配额，分配角色弹框，编辑功能，删除功能

--- src/views/systManagement/userManagement/index.vue
+++ src/views/systManagement/userManagement/index.vue
@@ -31,7 +31,7 @@
             >重置</el-button
           >
         </el-form-item>
-        <el-button type="primary" @click="handleAdd()" class="addBtn"
+        <el-button type="primary" @click="handleAdd({})" class="addBtn"
           >添加</el-button
         >
       </el-form>
@@ -46,16 +46,32 @@
         stripe
         border
       >
-        <el-table-column label="角色名称" prop="name" align="center">
+        <el-table-column
+          label="账号"
+          prop="account"
+          align="center"
+          width="100px"
+        >
+          <template slot-scope="scope">{{ scope.row.account }}</template>
+        </el-table-column>
+        <el-table-column label="姓名" prop="name" align="center" width="80px">
           <template slot-scope="scope">{{ scope.row.name }}</template>
         </el-table-column>
-        <el-table-column label="描述" align="center">
-          <template slot-scope="scope">{{ scope.row.description }}</template>
+        <el-table-column label="邮箱" prop="email" align="center">
+          <template slot-scope="scope">{{ scope.row.email }}</template>
         </el-table-column>
-        <el-table-column label="添加时间" align="center">
+        <el-table-column label="添加时间" align="center" prop="createTime">
           <template slot-scope="scope">{{ scope.row.createTime }}</template>
         </el-table-column>
-        <el-table-column label="是否启用" width="100px" align="center">
+        <el-table-column label="最后登陆" align="center" prop="endTime">
+          <template slot-scope="scope">{{ scope.row.endTime }}</template>
+        </el-table-column>
+        <el-table-column
+          label="是否启用"
+          width="100px"
+          align="center"
+          prop="status"
+        >
           <template slot-scope="scope">
             <el-switch
               @change="handleStatusChange(scope.$index, scope.row)"
@@ -70,24 +86,16 @@
           <template slot-scope="scope">
             <el-button
               size="mini"
-              @click="handleSelectMenu(scope.$index, scope.row)"
-              >分配菜单
+              @click="allocationQuota(scope.$index, scope.row)"
+              >分配配额
             </el-button>
-            <el-button
-              size="mini"
-              @click="handleSelectResource(scope.$index, scope.row)"
-              >分配资源
+            <el-button size="mini" @click="assignRoles(scope.$index, scope.row)"
+              >分配角色
             </el-button>
-
-            <el-button
-              size="mini"
-              @click="handleUpdate(scope.$index, scope.row)"
+            <el-button size="mini" @click="handleAdd(scope.row)"
               >编辑
             </el-button>
-            <el-button
-              size="mini"
-              type="danger"
-              @click="handleDelete(scope.$index, scope.row)"
+            <el-button size="mini" type="danger" @click="deleteItem(scope.row)"
               >删除
             </el-button>
           </template>
@@ -106,11 +114,33 @@
       >
       </el-pagination>
     </el-card>
+    <!-- 使用组件 -->
+    <addUser
+      :visible="detailState"
+      :detail="detail"
+      @cancel="detailState = false"
+    ></addUser>
+    <assignRoles
+      :roleVisible="roleVisible"
+      @cancel="roleVisible = false"
+    ></assignRoles>
+    <!-- 分配配额 -->
+    <allocationQuota
+      :alloVisible="alloVisible"
+      @cancel="alloVisible = false"
+    ></allocationQuota>
   </div>
 </template>
 
 <script>
+// 导入删除接口（以下接口仅为模拟，需根据实际状况导入)
+// import {
+//     delApplicationModule
+//   } from "@/api/systemManage/sysAppliationModule";
 import createBtn from "@/components/createBtn";
+import addUser from "./components/addUser.vue"; //添加用户
+import assignRoles from "./components/assignRoles.vue";
+import allocationQuota from "./components/allocationQuota.vue";
 const defaultListQuery = {
   pageNum: 1,
   pageSize: 10,
@@ -119,18 +149,106 @@ const defaultListQuery = {
 export default {
   name: "userManagement",
   components: {
-    createBtn
+    createBtn,
+    addUser,
+    assignRoles,
+    allocationQuota
   },
   data() {
     return {
+      detail: {},
+      detailState: false, //添加用户
+      roleVisible: false, //角色
+      alloVisible: false, //分配配额
       listQuery: Object.assign({}, defaultListQuery),
       listLoading: false,
-      list: [{ name: "zs" }]
+      list: [
+        {
+          id: 1,
+          account: "251501111",
+          name: "zs",
+          email: "2515011987@qq.com",
+          createTime: "2021-06-15",
+          endTime: "2021-06-17",
+          status: 1
+        },
+        {
+          id: 2,
+          account: "251501222",
+          name: "ls",
+          email: "2515011444@qq.com",
+          createTime: "2021-06-15",
+          endTime: "2021-06-17",
+          status: 0
+        },
+        {
+          id: 3,
+          account: "251501222",
+          name: "ls",
+          email: "2515011444@qq.com",
+          createTime: "2021-06-15",
+          endTime: "2021-06-17",
+          status: 0
+        },
+        {
+          id: 4,
+          account: "251501222",
+          name: "ww",
+          email: "2515011555@qq.com",
+          createTime: "2021-06-12",
+          endTime: "2021-06-17",
+          status: 1
+        }
+      ]
     };
   },
   created() {},
   mounted() {},
   methods: {
+    // 是否启用
+    handleStatusChange(data) {
+      console.log("status", data);
+    },
+    // 分配配额
+    allocationQuota() {
+      this.alloVisible = true;
+    },
+    // 分配角色
+    assignRoles() {
+      console.log("111");
+      this.roleVisible = true;
+    },
+    // 删除用户
+    deleteItem(data) {
+      console.log("删除", data);
+      // let params = {
+      //   id: data.id
+      // };
+      // this.$confirm(`确定要删除模块【${data.name}】吗？`, "删除系统应用模块", {
+      //   confirmButtonText: "确定",
+      //   cancelButtonText: "取消",
+      //   type: "warning"
+      // }).then(() => {
+      //   delApplicationModule(params)
+      //     .then(res => {
+      //       let data = res.data;
+      //       if (data.resultCode === 200) {
+      //         this.$message.success(data.data);
+      //         this.changePage(1);
+      //       } else {
+      //         this.$message.error(`删除失败`);
+      //       }
+      //     })
+      //     .catch(err => {
+      //       this.$message.error(err.response.data.msg);
+      //     });
+      // });
+    },
+    //添加编辑用户
+    handleAdd(data) {
+      this.detail = JSON.parse(JSON.stringify(data));
+      this.detailState = true;
+    },
     handleResetSearch() {
       this.listQuery = Object.assign({}, defaultListQuery);
     },
